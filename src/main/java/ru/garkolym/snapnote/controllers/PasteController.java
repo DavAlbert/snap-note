8b8d4b5a6d8ba832c89a363828a56f7720894e26
David Albert <garkolym@googlemail.com>
2019-08-20 08:04:40 +0200

    fix: problem with static files

--- src/main/java/ru/garkolym/snapnote/controllers/PasteController.java
+++ src/main/java/ru/garkolym/snapnote/controllers/PasteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PasteController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
