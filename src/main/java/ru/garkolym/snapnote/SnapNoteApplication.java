40b75b6ba8be03bdbe0949353ce366480548ae3f
David Albert <garkolym@googlemail.com>
2019-08-20 06:53:30 +0200

    init

---
+++ src/main/java/ru/garkolym/snapnote/SnapNoteApplication.java
@@ -0,0 +1,13 @@
+package ru.garkolym.snapnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapNoteApplication.class, args);
+	}
+
+}
